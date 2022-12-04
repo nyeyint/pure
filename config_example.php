c1d8f883d5beefa59cded39a11b1c2ac30dc165d
Ye Yint <yeyint@Yes-MacBook-Pro.local>
2022-12-04 14:19:42 +0800

    add gitignore

---
+++ config_example.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'host'      => 'localhost',
+        'post'      => 3306,
+        'dbname'    => 'db_pure',
+        'user'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8mb4'
+    ],
+
+    'services' => []
+];
